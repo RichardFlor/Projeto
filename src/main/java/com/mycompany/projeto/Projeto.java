94868c079a96adba623bc1a2008154a297933c3d
RichardFlor <richardflor2015@gmail.com>
2023-06-04 22:54:08 -0300

    70% concluido

--- src/main/java/com/mycompany/projeto/Projeto.java
+++ src/main/java/com/mycompany/projeto/Projeto.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 package com.mycompany.projeto;
 
 import com.mycompany.projeto.Login.Login;
